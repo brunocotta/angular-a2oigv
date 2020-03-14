2a6f440a9021b46647fe18684edaeb660bb5c1c1
brunocotta <brunocotta316@gmail.com>
2020-03-14 16:27:19 -0300

    4 files modified, 2 added

--- src/app/func1/func1.module.ts
+++ src/app/func1/func1.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {Comp1Component} from './comp1/comp1.component';
 
 @NgModule ({
 
-declarations: [], //Components do Móódulo...
-exports: [], // Components visííveis a outros móódulos
+declarations: [Comp1Component], //Components do Móódulo...
+exports: [Comp1Component], // Components visííveis a outros móódulos
 imports: [CommonModule], // Quais móódulos serãão importantes aqui...
 
 })
