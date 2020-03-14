b6b57596ab71c73b9eb3fc085a2a3607ba0f9394
brunocotta <brunocotta316@gmail.com>
2020-03-14 13:55:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-a2oigv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a2oigv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports : [], // Outros Móódulos Usados
+  exports: [], // Vou exportar (tronar visíível um compnente, pipe...
+  providers: [], // É o conjunto de serviços qu estarãão disponííveis para este móódulo...
+  declarations: [], // É o conjunto de serviços que estarãão disponííveis para este móódulo...
+  bootstrap: [] // É o conjunto de componentes pelo qual a aplicação web vai iniciar a execução
+
+})
+
+export class AppModule {
+
+
+}
